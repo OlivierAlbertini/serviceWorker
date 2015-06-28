779403961b9dd1627f2219f4d080a459881c1555
OlivierAlbertini <albertini.olivier@gmail.com>
2015-06-28 19:23:20 -0400

    init

---
+++ myserviceworker.js
@@ -0,0 +1,13 @@
+﻿if ('function' === typeof importScripts) {
+    importScripts('serviceworker.js');
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/Scripts/Service/sw.js').then(function (registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}

678dacdb1113cd4f080095312bad165378f4f593
OlivierAlbertini <albertini.olivier@gmail.com>
2015-06-28 19:49:54 -0400

    url

--- myserviceworker.js
+++ myserviceworker.js
@@ -3,7 +3,7 @@
 }
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/Scripts/Service/sw.js').then(function (registration) {
+    navigator.serviceWorker.register('https://cdn.rawgit.com/OlivierAlbertini/serviceWorker/master/sw.js').then(function (registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function (err) {
